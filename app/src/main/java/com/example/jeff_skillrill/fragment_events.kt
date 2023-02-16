5f678772df536f7e0195fa75ab17376daa3eb405
Acer <abdujafar.rashidov.2006@gmail.com>
2023-02-16 12:09:47 +0500

    Ready_Demo3

--- app/src/main/java/com/example/jeff_skillrill/fragment_events.kt
+++ app/src/main/java/com/example/jeff_skillrill/fragment_events.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_events.recycler_view
+import kotlinx.android.synthetic.main.fragment_events.*
 //import androidx.recyclerview.widget.LinearLayoutManager
 //import androidx.recyclerview.widget.RecyclerView
 //import kotlinx.android.synthetic.main.fragment_events.recyclerView
@@ -18,97 +22,26 @@ private const val ARG_PARAM2 = "param2"
 
 class fragment_events : Fragment() {
 
-//    private lateinit var eventRecyclerView: RecyclerView
-    private lateinit var newArrayList: ArrayList<Events>
-    lateinit var ImageId: Array<Int>
-    lateinit var about: Array<String>
-
-//    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-
-//        ImageId = arrayOf(
-//            R.drawable.topic_demo,
-//            R.drawable.topic_demo,
-//            R.drawable.topic_demo,
-//            R.drawable.topic_demo,
-//            R.drawable.topic_demo,
-//            R.drawable.topic_demo,
-//            R.drawable.topic_demo,
-//            R.drawable.topic_demo,
-//            R.drawable.topic_demo
-//        )
-//
-//        about = arrayOf(
-//            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//            "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-//
-//
-//        )
-//
-//        recyclerView.layoutManager = LinearLayoutManager(activity)
-//        recyclerView.setHasFixedSize(true)
-//
-//        newArrayList = arrayListOf<Events>()
-//        getUserData()
-
-
-
-    }
-
-//    private fun getUserData() {
-//        for (i in ImageId.indices)
-//        {
-//            val events = Events(ImageId[i],about[i])
-//            newArrayList.add(events)
-//        }
-//        recyclerView.adapter = MyAdapter(newArrayList)
-//    }
-
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<MyAdapter.ViewHolder>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-
-
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_events, container, false)
-
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_events.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            fragment_events().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(itemView, savedInstanceState)
+        recycler_view.apply {
+            // set a LinearLayoutManager to handle Android
+            // RecyclerView behavior
+            layoutManager = LinearLayoutManager(activity)
+            // set the custom adapter to the RecyclerView
+            adapter = MyAdapter()
+        }
     }
 
 
