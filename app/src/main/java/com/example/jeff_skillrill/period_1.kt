9127f05b21f396e158d2a38f3e77aaf5e7e00b5b
Acer <abdujafar.rashidov.2006@gmail.com>
2023-02-21 17:40:29 +0500

    demo_project_done

--- app/src/main/java/com/example/jeff_skillrill/period_1.kt
+++ app/src/main/java/com/example/jeff_skillrill/period_1.kt
@@ -1,50 +1,125 @@
 package com.example.jeff_skillrill
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.trendyol.bubblescrollbarlib.BubbleTextProvider
-import kotlinx.android.synthetic.main.activity_period1.bubble_scroll_ber
-import kotlinx.android.synthetic.main.activity_period1.recyclerView_period_1
+import com.example.jeff_skillrill.periodone.Event_one_adapter
+import com.example.jeff_skillrill.periodone.Event_one_data
+import java.util.ArrayList
+import java.util.Locale
+
 
 class period_1 : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
-    private lateinit var eventList: ArrayList<Events>
-    private lateinit var eventAdapter: EventAdapter
+    private lateinit var searchView: SearchView
+    private var mList = ArrayList<Event_one_data>()
+    private lateinit var adapter: Event_one_adapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_period1)
 
-        recyclerView = findViewById(R.id.recyclerView_period_1)
+        recyclerView = findViewById(R.id.recyclerView)
+        searchView = findViewById(R.id.searchView)
+
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = LinearLayoutManager(this)
+        addDataToList()
+        adapter = Event_one_adapter(mList)
+        recyclerView.adapter = adapter
 
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
 
-        eventList = ArrayList()
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filterList(newText)
+                return true
+            }
 
-        eventList.add(Events(R.drawable.topic_demo, "Topic_1"))
-        eventList.add(Events(R.drawable.topic_demo, "Topic_2"))
-        eventList.add(Events(R.drawable.topic_demo, "Topic_3"))
-        eventList.add(Events(R.drawable.topic_demo, "Topic_4"))
-        eventList.add(Events(R.drawable.topic_demo, "Topic_5"))
-        eventList.add(Events(R.drawable.topic_demo, "Topic_6"))
+        })
+    }
 
-        eventAdapter = EventAdapter(eventList)
-        recyclerView.adapter = eventAdapter
+    private fun filterList(query: String?) {
 
-        bubble_scroll_ber.attachToRecyclerView(recyclerView_period_1)
-        bubble_scroll_ber.bubbleTextProvider = BubbleTextProvider {
-            position -> StringBuilder(eventList[position].substring(0,1)).toString()
+        if (query != null) {
+            val filteredList = ArrayList<Event_one_data>()
+            for (i in mList) {
+                if (i.title.lowercase(Locale.ROOT).contains(query)) {
+                    filteredList.add(i)
+                }
+            }
+
+            if (filteredList.isEmpty()) {
+                Toast.makeText(this, "No Data found", Toast.LENGTH_SHORT).show()
+            } else {
+                adapter.setFilteredList(filteredList)
+            }
         }
+    }
 
-        eventAdapter.onItemClick = {
-            val intent = Intent(this, Detailed_Activity_period_1::class.java)
-            intent.putExtra("Event", it)
-            startActivity(intent)
-        }
+    private fun addDataToList() {
+        mList.add(
+            Event_one_data(
+                "Java",
+                R.drawable.topic_demo,
+                "Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible."
+            )
+        )
+        mList.add(
+            Event_one_data(
+                "Kotlin",
+                R.drawable.topic_demo,
+                "Kotlin is a cross-platform, statically typed, general-purpose programming language with type inference. Kotlin is designed to interoperate fully with Java, and the JVM version of Kotlin's standard library depends on the Java Class Library, but type inference allows its syntax to be more concise."
+            )
+        )
+        mList.add(
+            Event_one_data(
+                "HTML",
+                R.drawable.topic_demo,
+                "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser."
+            )
+        )
+        mList.add(
+            Event_one_data(
+                "Python",
+                R.drawable.topic_demo,
+                "Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation."
+            )
+        )
+        mList.add(
+            Event_one_data(
+                "C++",
+                R.drawable.topic_demo,
+                "C++ is a high-level general-purpose programming language created by Danish computer scientist Bjarne Stroustrup as an extension of the C programming language, or C with Classes."
 
+            )
+        )
+        mList.add(
+            Event_one_data(
+                "Swift",
+                R.drawable.topic_demo,
+                "Swift is a general-purpose, multi-paradigm, compiled programming language developed by Apple Inc. and the open-source community."
+            )
+        )
+        mList.add(
+            Event_one_data(
+                "JavaScript",
+                R.drawable.topic_demo,
+                "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 98% of websites use JavaScript on the client side for webpage behavior, often incorporating third-party libraries."
+            )
+        )
+        mList.add(
+            Event_one_data(
+                "C#",
+                R.drawable.topic_demo,
+                "C# is a general-purpose, high-level multi-paradigm programming language. C# encompasses static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented, and component-oriented programming disciplines."
+            )
+        )
     }
 }
 
---
+++ app/src/main/java/com/example/jeff_skillrill/periodone/Event_one_data.kt
@@ -0,0 +1,9 @@
+package com.example.jeff_skillrill.periodone
+
+data class Event_one_data
+    (
+    val title: String,
+    val logo: Int,
+    val desc: String,
+    var isExpandable: Boolean = false
+)
